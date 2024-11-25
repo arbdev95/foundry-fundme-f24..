02a3441d80511c9f130ca7e2107a3696ea4e966d
arbdev <ahmeddone@hotmail.it>
2024-11-25 08:09:34 +0100

    Fix case mismatch in Script.sol import

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -5,7 +5,7 @@
 
 pragma solidity ^0.8.18;
 
-import {Script} from "forge-std/SCript.sol";
+import {Script} from "forge-std/Script.sol";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
